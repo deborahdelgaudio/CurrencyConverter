7234a20fc1c961602a05e9a67bf58ef1c21b22a4
deborahdelgaudio <delgaudio.d@gmail.com>
2020-02-13 21:26:09 +0100

    add ConversionRate model

--- converter/convert/models.py
+++ converter/convert/models.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 
-# Create your models here.
+
+class ConversionRate(models.Model):
+    iso_currency = models.CharField(max_length=3, unique_for_date='date')
+    rate = models.DecimalField(max_digits=7, decimal_places=2)
+    date = models.DateField()
