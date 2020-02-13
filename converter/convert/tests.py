559ae28932b4a192526e311144bc98ff8bb4177d
deborahdelgaudio <delgaudio.d@gmail.com>
2020-02-14 00:34:51 +0100

    prepare testcases

--- converter/convert/tests.py
+++ converter/convert/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+# -*- coding: utf-8 -*-
+from rest_framework.test import APIRequestFactory
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class ConvertApiTestCase(APITestCase):
+    def test_endpoint_without_query_string_params(self):
+        pass
+
+    def test_conversion_without_a_required_param(self):
+        pass
+
+    def test_conversion_With_wrong_date_format(self):
+        pass
+
+    def test_conversion_With_future_date(self):
+        pass
+
+    def test_conversion_with_only_required_params(self):
+        pass
+
+    def test_conversion_all_params(self):
+        pass
+
+    def test_conversion_with_odd_params(self):
+        pass
